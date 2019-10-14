5534307d2d8585104f097e336ce30431b8727c5e
dbeattie <darren@fettle.ca>
2019-10-14 22:27:50 +0000

    Created a function that sliced the head off the front of an array to spit it out, relative to previous assertEqual.js function

---
+++ tail.js
@@ -0,0 +1,18 @@
+const tail = function(words) {
+let output = words.slice(1);
+console.log(output);
+}
+
+//copied over function from assertEqual.js
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}.`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} === ${expected}.`);
+  }
+};
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
