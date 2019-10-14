720594ac2c964c98f4000baae826f8513d6fc720
dbeattie <darren@fettle.ca>
2019-10-14 21:42:49 +0000

    Linted the previous bit of code

--- head.js
+++ head.js
@@ -1,7 +1,7 @@
 //copied over function from assertEqual.js
 const head = function(array) {
-  let output = array[0]
-  return output
+  let output = array[0];
+  return output;
 };
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
@@ -13,8 +13,3 @@ const assertEqual = function(actual, expected) {
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-//if (actual === expected) {
-      //console.log(`✅ Assertion Passed: ${actual} === ${expected}.`);
-    //} else {
-      //console.log(`🔴 Assertion Failed: ${actual} === ${expected}.`);
