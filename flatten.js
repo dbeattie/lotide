9bf16ee8d55fe7592fb28ab1bd00e39871a1c3d5
dbeattie <darren@fettle.ca>
2019-10-17 00:38:17 +0000

    created a function that took arrays from within arrays and added them back into the array as new elements

--- flatten.js
+++ flatten.js
@@ -31,3 +31,32 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 //NEW WITHOUT FUNCTION
+const flatten = function(arrInput) {
+  let newArr = [];
+  for (let i = 0; i < arrInput.length; i++) {
+    if (Array.isArray(arrInput[i]) === true) {
+      for (let x = 0; x < arrInput[i].length; x++) {
+        newArr.push(arrInput[i][x]);
+      }
+    } else {
+      newArr.push(arrInput[i]);
+    }
+  }
+  return newArr;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+console.log(flatten(['a', 'b', ['c', 'd', 'e'], 'f', ['g']]));
+
+// WILL RETURN ALL THE ARRAYS FROM WITHIN THE ARRAY
+// const flatten = function(arrInput) {
+//   let newArr = [];
+//   //if (Array.isArray) {
+//     for (let i = 0; i < arrInput.length; i++) {
+//       if (Array.isArray(arrInput[i]) === true) {
+//         newArr.push(arrInput[i]);
+//       }
+//     }
+//   //}
+//   return newArr;
+// }
