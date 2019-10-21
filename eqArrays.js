6c474fa1a7cda03b30004f40a2cf794869da63fa
dbeattie <darren@fettle.ca>
2019-10-21 17:09:01 +0000

    refactored test code for assertArraysEqual.js to test file

--- eqArrays.js
+++ eqArrays.js
@@ -11,19 +11,5 @@ const eqArrays = function(a, b) {
     return output;
   }
 };
-// copied over from assertEqual.js
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}.`);
-  }
-};
 
-// TEST CODE
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
