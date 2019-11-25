2d4efcd64104dd4d635acd590b6edf7382183efa
dbeattie <darren@fettle.ca>
2019-11-25 23:52:45 +0000

    updated my function to make it cleaner

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,34 @@
 const eqArrays = function(a, b) {
   let output;
-  if (a.length !== b.length) {
-    output = false;
-  } else {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        output = false;
-      } else output = true;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      output = false;
+    } else {
+      output = true;
     }
-    return output;
   }
-};
+  return output;
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
 module.exports = eqArrays;
+
+//INITIAL OLD FUNCTION
+// const eqArrays = function(a, b) {
+//   let output;
+//   if (a.length !== b.length) {
+//     output = false;
+//   } else {
+//     for (let i = 0; i < a.length; i++) {
+//       if (a[i] !== b[i]) {
+//         output = false;
+//       } else output = true;
+//     }
+//     return output;
+//   }
+// };
