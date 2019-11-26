094bf1963d1bfa2658e45f6db4ba63360552e77d
dbeattie <darren@fettle.ca>
2019-11-26 15:09:09 +0000

    updated flatten to better utilize assertArraysEqual and eqArrays

--- flatten.js
+++ flatten.js
@@ -1,40 +1,31 @@
-//logic taken from eqArrays
 const eqArrays = function(a, b) {
   let output;
-  if (a.length !== b.length) {
-    output = false;
-  } else {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        output = false;
-      } else output = true;
-    }
-    return output;
+  if ((a === undefined || a.length === 0) && (b === undefined || b.length === 0)) {
+    output = true;
   }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      output = false;
+    } else {
+      output = true;
+    }
+  }
+  return output;
 };
-//logic taken from assertArraysEqual.js
+
 const assertArraysEqual = function(actual, expected) {
-  let output;
-  if (actual.length !== expected.length) {
-    output = false;
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}.\n`);
   } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        output = false;
-      } else output = true;
-    }
-  }
-  if (output) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}.`);
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}.\n`);
   }
 };
-//NEW WITHOUT FUNCTION
+
+//NEW FLATTEN FUNCTION
 const flatten = function(arrInput) {
   let newArr = [];
   for (let i = 0; i < arrInput.length; i++) {
-    if (Array.isArray(arrInput[i]) === true) {
+    if (Array.isArray(arrInput[i])) {
       for (let x = 0; x < arrInput[i].length; x++) {
         newArr.push(arrInput[i][x]);
       }
@@ -45,20 +36,7 @@ const flatten = function(arrInput) {
   return newArr;
 };
 
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), ([ 1, 2, 3, 4, 5, 6 ]));
+assertArraysEqual(flatten(['a', 'b', ['c', 'd', 'e'], 'f', ['g']]), ([ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]));
+
 module.exports = flatten;
-
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-console.log(flatten(['a', 'b', ['c', 'd', 'e'], 'f', ['g']]));
-
-// WILL RETURN ALL THE ARRAYS FROM WITHIN THE ARRAY
-// const flatten = function(arrInput) {
-//   let newArr = [];
-//   //if (Array.isArray) {
-//     for (let i = 0; i < arrInput.length; i++) {
-//       if (Array.isArray(arrInput[i]) === true) {
-//         newArr.push(arrInput[i]);
-//       }
-//     }
-//   //}
-//   return newArr;
-// }
