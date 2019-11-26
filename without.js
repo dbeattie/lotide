bca902e3c9d4ca33270f35bf4b29806ddffcf387
dbeattie <darren@fettle.ca>
2019-11-26 18:39:08 +0000

    Created and tested a function that analyzes one array, to see if any exact items in a second array match, then remove those items from the first array, as a new array

--- without.js
+++ without.js
@@ -1,54 +1,38 @@
-//logic taken from eqArrays
 const eqArrays = function(a, b) {
   let output;
-  if (a.length !== b.length) {
-    output = false;
-  } else {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        output = false;
-      } else output = true;
+  if ((a === undefined || a.length === 0) && (b === undefined || b.length === 0)) {
+    output = true;
+   } 
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      output = false;
+    } else {
+      output = true;
     }
-    return output;
   }
-};
-//logic taken from assertArraysEqual.js
+  return output;
+}
+
 const assertArraysEqual = function(actual, expected) {
-  let output;
-  if (actual.length !== expected.length) {
-    output = false;
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}.\n`);
   } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        output = false;
-      } else output = true;
-    }
-  }
-  if (output) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}.`);
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}.\n`);
   }
 };
-//NEW WITHOUT FUNCTION
+
 const without = function(source, itemsToRemove) {
-  //console.log(source);
-  //console.log(itemsToRemove);
-  let newArr = [];
-  for (let i = 0; i < source.length; i++) {
-    for (let x = 0; x < itemsToRemove.length; x++) {
-      if (itemsToRemove[x] !== source[i]) {
-        newArr.push(source[i]);
-      }
+  let newArr = source;
+  for (let i = 0; i < newArr.length; i++) {
+    if (itemsToRemove.includes(newArr[i])) {
+      newArr.splice(i, 1);
     }
   }
   return newArr;
 };
 
-module.exports = without;
+assertArraysEqual(without([1, 2, 3, 4], [1, 4]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3", "4"], [1, 2, "3"]), ["1", "2", "4"]);
+assertArraysEqual(without(["hello", "world", "lighthouse"], ["lighthouse"]), ["hello", "world"]);
 
-console.log(without(["1", "2", "3"], [1, 2, "3"]));
-const words = ["hello", "world", "lighthouse"];
-console.log(without(["hello", "world", "lighthouse"], ["lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
