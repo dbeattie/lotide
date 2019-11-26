682c9d2cfdd5857b69b644050763da4547ddd37e
dbeattie <darren@fettle.ca>
2019-11-26 00:09:01 +0000

    Rafactored to shorten this code and added test cases

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,28 @@
-//Refactored over from assertEqual.js with logic taken from eqArrays.js
+//Refactored over from assertEqual.js with logic taken from assertArraysEqual.js
 const assertArraysEqual = function(actual, expected) {
   let output;
-  if (actual.length !== expected.length) {
-    output = false;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        output = false;
-      } else output = true;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      output = false;
+    } else {
+      output = true;
     }
   }
   if (output) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}.`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}.\n`);
   } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}.`);
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}.\n`);
   }
 };
 
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+
+
 module.exports = assertArraysEqual;
+
+//FIRST PART OF THE FUNCTION PULLED FROM eqArrays.js
+//SECOND PART PULLED FROM assertEqual.js
+//SEE THOSE FOR PREVIOUS IDEAS, THIS IS A SHORTER IMPROVED VERSION
