7a46cae6221ae1497e862d6f7c7b73d9a763ebb8
dbeattie <darren@fettle.ca>
2019-11-26 20:53:58 +0000

    created a function that finds a key value in an object, based on a second parameter and returns the associated value.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,26 +1,29 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}.`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}.`);
   } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}.`);
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}.`);
   }
 };
 
-function findKeyByValue(object, value) {
-  let keys = Object.keys(object);
-  for (let key of keys) {
-    if (object[key] === value) {
-      return key;
+const findKeyByValue = function(object, value) {
+  let returnValue;
+  let objKeys = Object.keys(object);
+  
+  for (let objKey of objKeys) {
+    if (object[objKey] === value) {
+      returnValue = objKey;
     }
   }
-}
+  return returnValue;
+};
 
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
