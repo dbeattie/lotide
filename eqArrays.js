c4aaa5a28a80467378ff684748294992938fd946
dbeattie <darren@fettle.ca>
2019-12-02 18:30:23 +0000

    updated eqArrays.js to account for Mocha/Chai tests in eqArraysTest.js
    Removed manual tests from eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -16,39 +16,4 @@ const eqArrays = function(a, b) {
   return output;
 }
 
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-console.log(eqArrays(["2", 3], ["2", 3, 4])); // => false
-
 module.exports = eqArrays;
-
-
-//SIMPLIEST VERSION I COULD COME UP WITH
-// const eqArrays = function(a, b) {
-//   let output;
-//   if (JSON.stringify(a) === JSON.stringify(b)) {
-//     output = true;
-//   } else {
-//     output = false;
-//   }
-//   return output;
-// };
-
-
-//INITIAL OLD FUNCTION
-// const eqArrays = function(a, b) {
-//   let output;
-//   if (a.length !== b.length) {
-//     output = false;
-//   } else {
-//     for (let i = 0; i < a.length; i++) {
-//       if (a[i] !== b[i]) {
-//         output = false;
-//       } else output = true;
-//     }
-//     return output;
-//   }
-// };
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,10 +1,27 @@
 const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
-// TEST CODE
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+describe("#eqArrays", () => {
+  
+  it("[1, 2, 3] equals [1, 2, 3] -> Should be TRUE", () => {
+    assert.deepStrictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  
+  it("[1, 2, 3] does not equals [3, 2, 1] -> Should be FALSE", () => {
+    assert.deepStrictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
+  });
+
+  it("['1', '2', '3'] and ['1', '2', '3'] are equal -> TRUE", () => {
+    assert.deepStrictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
+  });
+
+  it("['1', '2', '3'] and ['1', '2', 3] are not equal -> FALSE", () => {
+    assert.deepStrictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
+  });
+
+  it("['2', 3] and ['2', 3, 4] are not equal -> FALSE", () => {
+    assert.deepStrictEqual(eqArrays(["2", 3], ["2", 3, 4]), false); 
+  });
+
+});
